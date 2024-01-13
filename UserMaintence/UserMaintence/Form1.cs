0a18209fc260ca8245c6b4009acf0625dcfef4bf
hegdavid01 <hegdavid2001@gmail.com>
2024-01-13 20:32:19 +0100

    teljesnev

--- UserMaintence/UserMaintence/Form1.cs
+++ UserMaintence/UserMaintence/Form1.cs
@@ -18,8 +18,7 @@ namespace UserMaintence
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName; 
-            label2.Text = Resource1.FirstName; 
+            label1.Text = Resource1.FullName;  
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -28,8 +27,7 @@ namespace UserMaintence
 
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
             };
             users.Add(u);
         }
