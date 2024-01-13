ec01babaa76238fb54a5f3c074ce1820910013c4
hegdavid01 <hegdavid2001@gmail.com>
2024-01-13 20:33:49 +0100

    teljesnevjavitas

--- UserMaintence/UserMaintence/Form1.Designer.cs
+++ UserMaintence/UserMaintence/Form1.Designer.cs
@@ -30,9 +30,7 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -52,14 +50,6 @@
             this.textBox1.Size = new System.Drawing.Size(205, 38);
             this.textBox1.TabIndex = 1;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(584, 62);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(205, 42);
-            this.textBox2.TabIndex = 2;
-            // 
             // label1
             // 
             this.label1.Location = new System.Drawing.Point(411, 21);
@@ -68,17 +58,9 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(411, 65);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 39);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(414, 119);
+            this.button1.Location = new System.Drawing.Point(414, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(375, 43);
             this.button1.TabIndex = 5;
@@ -91,9 +73,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -107,9 +87,7 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
     }
 }
