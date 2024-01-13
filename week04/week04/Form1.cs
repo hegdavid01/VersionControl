526c8ebce6264d5695df303e940001d3a5c19ec7
hegdavid01 <hegdavid2001@gmail.com>
2024-01-13 22:11:44 +0100

    adatbetoltes

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,17 @@ namespace week04
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+    
+        private void LoadData()
+        {
+            Flats = Context.Flats.ToList();
         }
     }
 }
