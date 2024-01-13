6e575bb41ce5c12b9f27be7e6f30c2fe21221943
hegdavid01 <hegdavid2001@gmail.com>
2024-01-13 22:38:09 +0100

    createexcel

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -26,11 +26,39 @@ namespace week04
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
     
         private void LoadData()
         {
             Flats = Context.Flats.ToList();
+        }
+
+        private void CreateExcel() 
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable(); 
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 
