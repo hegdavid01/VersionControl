a84c22495c9330689fb6fc1f08e4987bffe8ae8e
hegdavid01 <hegdavid2001@gmail.com>
2024-01-14 03:16:42 +0100

    javitas

--- week05/week05/Entities/RateData.cs
+++ week05/week05/Entities/RateData.cs
@@ -8,5 +8,8 @@ namespace week05.Entities
 {
     internal class RateData
     {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
     }
 }
