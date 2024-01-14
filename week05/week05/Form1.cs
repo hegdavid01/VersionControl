fa67bc8a078d9dd7fae5cbb5428dc66b042c6d69
hegdavid01 <hegdavid2001@gmail.com>
2024-01-14 01:26:49 +0100

    getexchangerate

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.MnbServiceReference;
 
 namespace week05
 {
@@ -16,5 +17,20 @@ namespace week05
         {
             InitializeComponent();
         }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult.ToString();
+        }
+
     }
 }
