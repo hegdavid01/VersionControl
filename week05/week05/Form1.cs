2dffc80929d0f04b3e5d60e802dc8b4fbc4b0e62
hegdavid01 <hegdavid2001@gmail.com>
2024-01-14 02:56:31 +0100

    dinamikus

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -23,8 +23,12 @@ namespace week05
         public Form1()
         {
             InitializeComponent();
-            DataGridView dataGridView1 = new DataGridView();
-            Controls.Add(dataGridView1);
+            RefreshData();            
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             dataGridView1.DataSource = Rates;
             XML();
             Diagram();
@@ -35,9 +39,9 @@ namespace week05
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()    
             };
             
             var response = mnbService.GetExchangeRates(request);
@@ -87,6 +91,19 @@ namespace week05
             chartArea.AxisY.IsStartedFromZero = false;
         }
 
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
 
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
