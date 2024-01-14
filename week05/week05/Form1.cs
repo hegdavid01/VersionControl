4cfc7814d14491a3946539e4061804fa1f02b818
hegdavid01 <hegdavid2001@gmail.com>
2024-01-14 01:46:00 +0100

    arfolyamlista

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.Entities;
 using week05.MnbServiceReference;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+
+            DataGridView dataGridView1 = new DataGridView();
+            Controls.Add(dataGridView1);
+            dataGridView1.DataSource = Rates;
         }
 
         private void GetExchangeRates()
@@ -31,6 +38,7 @@ namespace week05
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult.ToString();
         }
+
 
     }
 }
