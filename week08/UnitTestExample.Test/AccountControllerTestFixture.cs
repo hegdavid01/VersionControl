393108f99f724a0331722c41300527a86ec09782
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2024-01-15 21:18:50 +0100

    test

---
+++ week08/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
