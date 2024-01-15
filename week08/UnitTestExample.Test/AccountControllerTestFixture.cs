cfac9e5e39458907f659abdf7c7d79c88a48cbf5
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2024-01-15 21:23:24 +0100

    jelszo

--- week08/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week08/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -9,12 +9,27 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+       Test,
+       TestCase("abcd1234", false),
+       TestCase("irf@uni-corvinus", false),
+       TestCase("irf.uni-corvinus.hu", false),
+       TestCase("irf@uni-corvinus.hu", true)
+       ]
+
         public void TestValidateEmail(string email, bool expectedResult)
         {
             var accountController = new AccountController();
             var actualResult = accountController.ValidateEmail(email);
             Assert.AreEqual(expectedResult, actualResult);
         }
-    }
+           [Test,
+           TestCase("asdfASDF", false),
+           TestCase("ASDF1234", false),
+           TestCase("asdf1234", false),
+           TestCase("asd123", false),
+           TestCase("Asdf1234", true),
+           ]
+
+    }   
 }
