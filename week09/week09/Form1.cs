11fe696a474c2ba374594419d9bded4a7024be48
Dávid Hegedűs <hegdavid2001@gmail.com>
2024-01-15 22:57:11 +0100

    adatutolso

--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -15,10 +15,15 @@ namespace week09
     public partial class Form1 : Form
     {
         List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
         public Form1()
         {
             InitializeComponent();
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -38,5 +43,7 @@ namespace week09
                     });
                 }
             }
+
+            return population;
         }
 }
